71b95bbb51d8b2d32271fa8f7ca9ae6c3000d81d
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2023-03-06 20:18:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 echo "Do not run this with sudo!"
 sudo apt update
-sudo apt install git -y
+sudo apt install wget -y
 echo "Downloading..."
-git clone https://github.com/BastelPichi/berkely-db-fixed
-cd berkely-db-fixed/build_unix
+wget https://github.com/BastelPichi/berkely-db-fixed/releases/download/fix/db-4.8.30.NC.tar.gz
+tar xf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix
 ../dist/configure -q --disable-shared --enable-cxx --disable-replication --with-pic --prefix=/opt/local/db-4.8.30.NC
 make
 sudo make install
